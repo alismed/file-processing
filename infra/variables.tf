c84de45546c48e110afb49e1a8285ff40263dcbd
Alison Souza <alisonmsouza@yahoo.com.br>
2025-05-02 23:14:29 -0300

    dynamodb

--- infra/variables.tf
+++ infra/variables.tf
@@ -28,4 +28,33 @@ variable "storage_class" {
   default     = "STANDARD_IA"
 }
 
+variable "table_name" {
+  description = "The name of the DynamoDB table"
+  type        = string
+}
+
+variable "billing_mode" {
+  description = "The billing mode for the DynamoDB table"
+  type        = string
+  default     = "PAY_PER_REQUEST"
+}
+
+variable "ttl_attribute" {
+  description = "The attribute name for TTL in the DynamoDB table"
+  type        = string
+  default     = "ttl"
+}
+
+variable "gsi_config" {
+  description = "Configuration for global secondary indexes"
+  type = map(object({
+    hash_key           = string
+    write_capacity     = number
+    read_capacity      = number
+    projection_type    = string
+    non_key_attributes = list(string)
+  }))
+  default = {}
+}
+
 
