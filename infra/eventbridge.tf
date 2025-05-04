39a4ae7b10ed5ec9b473ec750757d056f9f35c30
Alison Souza <alisonmsouza@yahoo.com.br>
2025-05-03 21:14:59 -0300

    improvements

--- infra/eventbridge.tf
+++ infra/eventbridge.tf
@@ -1,13 +1,15 @@
 resource "aws_cloudwatch_event_rule" "s3_upload" {
-  name        = "capture-s3-uploads"
+  name        = var.event_rule_name
   description = "Capture all S3 object uploads"
 
   event_pattern = jsonencode({
     source      = ["aws.s3"]
-    detail-type = ["Object Created"]
+    detail-type = ["AWS API Call via CloudTrail"]
     detail = {
-      bucket = {
-        name = [aws_s3_bucket.file_bucket.id]
+      eventSource = ["s3.amazonaws.com"]
+      eventName   = ["PutObject", "CompleteMultipartUpload"]
+      requestParameters = {
+        bucketName = [aws_s3_bucket.file_bucket.id]
       }
     }
   })
@@ -15,6 +17,14 @@ resource "aws_cloudwatch_event_rule" "s3_upload" {
 
 resource "aws_cloudwatch_event_target" "s3_upload_target" {
   rule      = aws_cloudwatch_event_rule.s3_upload.name
-  target_id = "ProcessS3Upload"
+  target_id = var.event_target_id
   arn       = aws_lambda_function.file_processing.arn
+}
+
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowEventBridgeInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.file_processing.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.s3_upload.arn
 }
